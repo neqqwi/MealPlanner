92ece7b8863609cf640f31fe01077bdced2cc83b
Yana Botsura <yana.botsura0@gmail.com>
2026-06-05 20:56:10 +0300

    feat: add Recipe model and controller with static data

---
+++ MealPlanner/Controllers/RecipeController.cs
@@ -0,0 +1,33 @@
+﻿using MealPlanner.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MealPlanner.Controllers
+{
+    public class RecipeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Recipe> recipes = new List<Recipe>();
+
+            recipes.Add (new Recipe(1, "Лимонно-розмариновый цыпленок",
+            "Сочная курица, запеченная с чесноком, свежим розмарином и" +
+            " дольками лимона.", 90, "", "/images/lemon-rosemary-chicken.jpg"));
+
+            recipes.Add(new Recipe(2, "Шоколадно-вишневый фондан",
+            "Французский десерт с хрустящей корочкой снаружи и горячей, " +
+            "тягучей шоколадной начинкой, в центре которой прячется ягода " +
+            "вишни.", 120, "", "/images/chocolate-cherry-fondant.jpg"));
+
+            recipes.Add(new Recipe(3, "Крамбл с яблоками и корицей",
+            "Ароматный десерт из запеченных с корицей яблок, покрытых " +
+            "хрустящей крошкой из овсяных хлопьев, муки и сливочного масла.",
+            40, "", "/images/apple-crumble-pie.jpg"));
+
+            recipes.Add(new Recipe(4, "Тайский салат с говядиной",
+            "Сочные кусочки быстро обжаренного мяса с салатом, огурцом и кинзой" +
+            " под острой заправкой из чили и лайма", 25, "", "/images/thai-beef-salad.jpg"));
+
+            return View(recipes);
+        }
+    }
+}
---
+++ MealPlanner/Models/Recipe.cs
@@ -0,0 +1,35 @@
+﻿namespace MealPlanner.Models
+{
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int CookingTime { get; set; } // в минутах
+        public string Instructions { get; set; } = string.Empty;
+        public string? ImageUrl { get; set; }
+        public Recipe()
+        {
+            Name = string.Empty;
+            Description = string.Empty;
+            Instructions = string.Empty;
+        }
+        public Recipe(int id, string name, string description, int time, string instructions, string url)
+        {
+            RecipeId = id;
+            Name = name;
+            Description = description;
+            CookingTime = time;
+            Instructions = instructions;
+            ImageUrl = url;
+        }
+        public Recipe(int id, string name, string description, int time, string instructions)
+        {
+            RecipeId = id;
+            Name = name;
+            Description = description;
+            CookingTime = time;
+            Instructions = instructions;
+        }
+    }
+}
