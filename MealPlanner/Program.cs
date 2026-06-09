47a46eb35bb60401cef1afa83e9691fd5b6c699e
Yana Botsura <yana.botsura0@gmail.com>
2026-06-09 13:42:59 +0300

    feat: add recipe list page with Tailwind styling

--- MealPlanner/Program.cs
+++ MealPlanner/Program.cs
@@ -22,7 +22,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Recipe}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
