7bef44f4121327d3d97a6e51dbfb60f28fe04844
Yana Botsura <yana.botsura0@gmail.com>
2026-06-10 17:25:50 +0300

    feat: add AppDbContext with DbSets and relationship configuration

---
+++ MealPlanner/Data/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using MealPlanner.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealPlanner.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Recipe> Recipes { get; set; } = null!;
+        public DbSet<Ingredient> Ingredients { get; set; } = null!;
+        public DbSet<RecipeIngredient> RecipeIngredients { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasKey(ri => new { ri.RecipeId, ri.IngredientId });
+
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasOne(ri => ri.Recipe)
+                .WithMany(r => r.RecipeIngredients)
+                .HasForeignKey(ri => ri.RecipeId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasOne(ri => ri.Ingredient)
+                .WithMany(i => i.RecipeIngredients)
+                .HasForeignKey(ri => ri.IngredientId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
+    }
+}
